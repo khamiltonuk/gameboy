c78aacc6f1f76ede3f6931676f636d90b88e70b0
khamiltonuk <krisham87@hotmail.com>
2018-04-26 20:59:08 +0200

    production has css in seperate file

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,36 +1,33 @@
-var path = require('path')
-var webpack = require('webpack')
-var CleanWebpackPlugin = require('clean-webpack-plugin')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
+var path = require("path");
+var webpack = require("webpack");
+var CleanWebpackPlugin = require("clean-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 // Phaser webpack config
-var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/')
-var phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
-var pixi = path.join(phaserModule, 'build/custom/pixi.js')
-var p2 = path.join(phaserModule, 'build/custom/p2.js')
+var phaserModule = path.join(__dirname, "/node_modules/phaser-ce/");
+var phaser = path.join(phaserModule, "build/custom/phaser-split.js");
+var pixi = path.join(phaserModule, "build/custom/pixi.js");
+var p2 = path.join(phaserModule, "build/custom/p2.js");
 
 var definePlugin = new webpack.DefinePlugin({
-  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'false'))
-})
+  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || "false"))
+});
 
 module.exports = {
   entry: {
-    app: [
-      'babel-polyfill',
-      path.resolve(__dirname, 'src/main.js')
-    ],
-    vendor: ['pixi', 'p2', 'phaser', 'webfontloader']
-
+    app: ["babel-polyfill", path.resolve(__dirname, "src/main.js")],
+    vendor: ["pixi", "p2", "phaser", "webfontloader"]
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: './',
-    filename: 'js/bundle.js'
+    path: path.resolve(__dirname, "build"),
+    publicPath: "./",
+    filename: "js/bundle.js"
   },
   plugins: [
     definePlugin,
-    new CleanWebpackPlugin(['build']),
+    new CleanWebpackPlugin(["build"]),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new webpack.optimize.UglifyJsPlugin({
       drop_console: true,
@@ -39,12 +36,18 @@ module.exports = {
         comments: false
       }
     }),
-    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' /* chunkName= */ , filename: 'js/vendor.bundle.js' /* filename= */ }),
+    new ExtractTextPlugin("css/style.css", {
+      allChunks: true
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor" /* chunkName= */,
+      filename: "js/vendor.bundle.js" /* filename= */
+    }),
     new HtmlWebpackPlugin({
-      filename: 'index.html', // path.resolve(__dirname, 'build', 'index.html'),
-      template: './src/index.html',
-      chunks: ['vendor', 'app'],
-      chunksSortMode: 'manual',
+      filename: "index.html", // path.resolve(__dirname, 'build', 'index.html'),
+      template: "./src/index.html",
+      chunks: ["vendor", "app"],
+      chunksSortMode: "manual",
       minify: {
         removeAttributeQuotes: true,
         collapseWhitespace: true,
@@ -57,28 +60,34 @@ module.exports = {
       },
       hash: true
     }),
-    new CopyWebpackPlugin([
-      { from: 'assets', to: 'assets' }
-    ])
+    new CopyWebpackPlugin([{ from: "assets", to: "assets" }])
   ],
   module: {
     rules: [
-      { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
-      { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
-      { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
-      { test: /p2\.js/, use: ['expose-loader?p2'] }
+      {
+        test: /\.js$/,
+        use: ["babel-loader"],
+        include: path.join(__dirname, "src")
+      },
+      { test: /pixi\.js/, use: ["expose-loader?PIXI"] },
+      { test: /phaser-split\.js$/, use: ["expose-loader?Phaser"] },
+      { test: /p2\.js/, use: ["expose-loader?p2"] },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("css-loader!sass-loader")
+      }
     ]
   },
   node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
+    fs: "empty",
+    net: "empty",
+    tls: "empty"
   },
   resolve: {
     alias: {
-      'phaser': phaser,
-      'pixi': pixi,
-      'p2': p2
+      phaser: phaser,
+      pixi: pixi,
+      p2: p2
     }
   }
-}
+};
